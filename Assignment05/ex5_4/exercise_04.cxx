8b3b1e815ce1c86ff3f97d4790e1ebdbf3a86d7c
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-05-26 20:39:00 +0200

    finish basic requirement of exercise5_4

--- Assignment05/ex5_4/exercise_04.cxx
+++ Assignment05/ex5_4/exercise_04.cxx
@@ -22,7 +22,18 @@ void exercise_04::SimpleExecute(vtkImageData* input, vtkImageData* output)
 
 	// Step 1: Retrieve Information about the Data
 	// Get the size of your regular input grid, so you know your loop constraints and identify boundaries
+    // get the boundary xmin, xmax, ymin, ymax, zmin, zmax
+    int *inExt = input->GetExtent();
+    int xmin = inExt[0];
+	int xmax = inExt[1];
+	int ymin = inExt[2];
+	int ymax = inExt[3];
+	int zmin = inExt[4];
+	int zmax = inExt[5]
 
+	// get x, y, z dimensions
+	int *dims = input->GetDimensions();
+	int sliceSize = dims[0] * dims[1];
 	//...
 
 	// The following line accesses the data in the input object.
@@ -32,7 +43,12 @@ void exercise_04::SimpleExecute(vtkImageData* input, vtkImageData* output)
 
 	// Step 2: Allocate Output Memory
 	// Allocate an array to store gradient values, similar to last exercise. Use "Float" precision.
-
+	vtkSmartPointer<vtkFloatArray> gradientVector = vtkSmartPointer<vtkFloatArray>::New();
+    gradientVector->Initialize();
+    gradientVector->SetName("Gradient");
+    gradientVector->SetNumberOfComponents(3);
+    gradientVector->SetNumberOfTuples(input->GetNumberOfPoints());
+    gradientVector->FillValue(0.0f); // initialize the result array with zeros.
 	//...
 
 	// Step 3: Main Loop
@@ -41,11 +57,36 @@ void exercise_04::SimpleExecute(vtkImageData* input, vtkImageData* output)
 	// To do this, use the access funtions of the vtkUnsignedShortArray to get the data values.
 	// The gradient array you allocated also provides similar functions to store the data.
 	// Remember that you can simply set the boundary points' gradients to zero.
-	
+
+	// skip the boundaries in loop since all points are already initialized with zeros
+    // Traverse input data and calculate gradient
+
+    for (vtkIdType k = (zmin +1); k < zmax; k += 1) {
+        int kOffset = (k - zmin) * sliceSize;
+
+        for (vtkIdType j = (ymin + 1); j < ymax; j += 1) {
+            int jOffset = (j - ymin) * dims[0];
+
+            for (vtkIdType i = (xmin + 1); i < xmax; i += 1) {
+
+                // i +/- 1, j +/- dims[0], k +/i dims[0]*dims[1]
+                int diff[3] = {1, dims[0], sliceSize}
+                int index = (i - xmin) + jOffset + kOffset;
+                // getting f(i-1, j, k), f(i+1, j, k)
+                for (int comIndex = 0; comIndex < 3; comIndex += 1) {
+                    unsigned short int valuePlus = scalarData->GetValue(index + diff[comIndex]);
+                    unsigned short int valueMinus = scalarData->GetValue(index - diff[comIndex]);
+                    float gradient = (float) (valuePlus - valueMinus) / 2;
+                    gradientVector->SetComponent(index, comIndex, gradient);
+                }
+            }
+        }
+	}
+
 	//...
 
 	// Step 4: Add Calculation Results to Output
 	// Add the newly established gradient array to the point data of the output object, similar to last exercise.
-
+    output->GetPointData()->AddArray(gradientVector);
 	//...
 }
